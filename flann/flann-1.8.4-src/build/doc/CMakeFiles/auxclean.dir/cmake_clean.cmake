612e7ca0d673b9b486226d26631d421738bfc73f
nchavali <neelima.chavali@gmail.com>
2014-08-28 06:00:35 -0400

    Removing flann for now

--- flann/flann-1.8.4-src/build/doc/CMakeFiles/auxclean.dir/cmake_clean.cmake
+++
@@ -1,8 +0,0 @@
-FILE(REMOVE_RECURSE
-  "CMakeFiles/auxclean"
-)
-
-# Per-language clean rules from dependency scanning.
-FOREACH(lang)
-  INCLUDE(CMakeFiles/auxclean.dir/cmake_clean_${lang}.cmake OPTIONAL)
-ENDFOREACH(lang)
